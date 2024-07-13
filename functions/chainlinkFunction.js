043f71f0376d8de4fe6c8b354186a679758a7e5a
gasperpre <49305355+gasperpre@users.noreply.github.com>
2024-07-14 01:04:45 +0200

    update function

--- functions/chainlinkFunction.js
+++ functions/chainlinkFunction.js
@@ -4,23 +4,9 @@ const ethers = await import("npm:ethers@6.10.0");
 const company = args[0];
 const registrationNumber = company.slice(0, -2);
 
-// test creds
-const clientId = "3c5f94b7-ade9-43ef-afff-bf0fa33f3127";
-const clientSecret = "tEaBp1X8Qc4Gk4UhM6XWfJKMTRNZQZw7rMZpvHJV955N5iWY824O8K8Umw2G9mVu";
-
-// TODO: move authentication to backend
 const authRes = await Functions.makeHttpRequest({
-    method: 'POST',
-    url: 'https://login.bisnode.com/sandbox/v1/token.oauth2',
-    headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    data: {
-        client_id: clientId,
-        client_secret: clientSecret,
-        grant_type: "client_credentials",
-        scope: "credit_data_companies"
-    },
+    // TODO: replace with live API url
+    url: 'http://localhost:3000/api/dnbToken'
 })
 
 if (!authRes || !authRes.data) {
@@ -28,7 +14,6 @@ if (!authRes || !authRes.data) {
 }
 
 const access_token = authRes.data.access_token;
-
 const res = await Functions.makeHttpRequest({
     method: 'POST',
     url: 'https://login.bisnode.com/sandbox/v1/token.oauth2',
@@ -44,7 +29,7 @@ const res = await Functions.makeHttpRequest({
     })
 });
 
-if (!authRes || !authRes.data) {
+if (!res || !res.data) {
     throw Error("Failed to get report");
 }
 
