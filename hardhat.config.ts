7bf33e559bd39202f7ae79dde3c6c2202bbf4bec
gasperpre <49305355+gasperpre@users.noreply.github.com>
2024-07-14 06:22:06 +0200

    add base sepolia

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,14 +3,19 @@ import "@nomicfoundation/hardhat-toolbox";
 import './tasks/deploy';
 
 const INFURA_API_KEY = vars.get("INFURA_API_KEY");
+const TEST_PK = vars.get("TEST_PK");
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
-  defaultNetwork: 'sepolia',
+  defaultNetwork: 'base-sepolia',
   networks: {
     sepolia: {
       url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
-      accounts: [vars.get("TEST_PK")],
+      accounts: [TEST_PK],
+    },
+    basesepolia: {
+      url: `https://base-sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [TEST_PK],
     },
   },
 };
--- networks.ts
+++ networks.ts
@@ -3,6 +3,10 @@ const networks: Record<string, { functionsRouter: string, donId: string }> = {
         functionsRouter: '0xb83E47C2bC239B3bf370bc41e1459A34b41238D0',
         donId: '0x66756e2d657468657265756d2d7365706f6c69612d3100000000000000000000'
     },
+    basesepolia: {
+        functionsRouter: '0xf9B8fc078197181C841c296C876945aaa425B278',
+        donId: '0x66756e2d626173652d7365706f6c69612d310000000000000000000000000000'
+    },
 };
 
 export default networks;
